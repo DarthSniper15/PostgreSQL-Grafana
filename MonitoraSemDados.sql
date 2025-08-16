ec0d500da27315197c9b3d75ca6d2aeea9a86f23
Στυλό <60885200+DarthSniper15@users.noreply.github.com>
2025-08-16 14:40:02 -0300

    Create MonitoraSemDados.sql

---
+++ MonitoraSemDados.sql
@@ -0,0 +1,48 @@
+-- Query para Monitorar o alerta em tempos específicos --
+
+-- Define as variáveis
+WITH parametros AS (
+  SELECT    
+    '08:00'::time AS hora_inicio, -- Inicio do Intervalo de monitoramento
+    '17:00'::time AS hora_fim, --  Fim do Intervalo de monitoramento
+    INTERVAL '6 minutes' AS limite_tolerado, -- Limite tolerado sem dados
+    'tagname' AS tag -- Tagname do sistema
+    
+),
+
+-- Pegas os dados brutos
+ultimo_dado AS (
+  SELECT
+    "timestamp", 
+    value
+    FROM database, parametros p
+    WHERE tagname = p.tag AND $__timeFilter("timestamp")
+    ORDER BY "timestamp" DESC -- Ordena do mais recente para o mais antigo
+    LIMIT 1 -- Pega apenas o primeiro (o mais recente)
+)
+
+-- Responsável pelo alerta sem dados | pega o alerta do período mostrado no gráfico
+SELECT
+  NOW()::timestamptz AS time,
+  CASE
+  
+    -- Caso valores sejam nulos dentro do intervalo, alerta será definido como 1
+    WHEN (NOW()::timestamptz AT TIME ZONE 'America/Sao_Paulo')::time NOT BETWEEN p.hora_inicio AND p.hora_fim THEN
+            CASE
+                -- Condição de Alerta:
+                -- 1. Se não houver nenhum dado (último timestamp é NULL)
+                -- 2. OU se o último dado for NULL E o tempo for mais antigo que o limite de tolerado
+                WHEN u."timestamp" IS NULL 
+                OR (NOW()::timestamptz - u."timestamp" > p.limite_tolerado)
+                THEN 1
+                
+                -- Há dados dentro do limite ou do intervalo, alerta definido como 0
+                ELSE 0
+            END
+            
+        -- Fora do horário de monitoramento
+        ELSE NULL
+
+  END AS "alerta"
+  FROM parametros p
+  LEFT JOIN ultimo_dado u ON TRUE;
